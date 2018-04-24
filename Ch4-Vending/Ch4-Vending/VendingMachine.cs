b1e5aad2232cdfce3cda5ec6c4d260c1d03d0911
ABardot <aabardot5@gmail.com>
2018-04-24 18:10:56 -0400

    Added methods and calculations logic for money.

--- Ch4-Vending/Ch4-Vending/VendingMachine.cs
+++ Ch4-Vending/Ch4-Vending/VendingMachine.cs
@@ -8,6 +8,31 @@ namespace Ch4_Vending
 {
     class VendingMachine
     {
-        int DepositedAmount;
+        private int depositedAmount;
+        const int COST_OF_DRINK = 75; // Set the cost of the beverage in order to prevent magic numbers and cost can be changes once. 
+
+        public VendingMachine()
+        {
+            depositedAmount = 0;
+        }
+
+        public void DepositCoin(int coinAmount)
+        {
+            depositedAmount = coinAmount;
+        }
+
+        public void getDrink()
+        {
+            if (depositedAmount >= 75)
+            {    // This will calculation the amount deposited - cost of beverage 
+                Console.WriteLine("Your change is {0:} cents", depositedAmount - COST_OF_DRINK);
+                DepositCoin(0); // Call the method to set deposited amount to Zero.
+            }
+            else
+            {
+
+            }
+        }
+
     }
 }
